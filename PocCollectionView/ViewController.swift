558cca4abbcc24ccca350aa1cc763a09ac44d7c4
Nicolas Rios <nicolas.rios25@yahoo.com>
2022-10-30 20:20:08 -0700

    Initial Commit

---
+++ PocCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PocCollectionView
+//
+//  Created by Nicolas Rios on 10/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
